422b0905e4e5fb94a3a62126d2c55c6683797190
chikchilcy <chikchicly@gmail.com>
2024-08-08 10:00:21 +0500

    FINALLY

--- script.js
+++ script.js
@@ -1,25 +1,72 @@
-function first(){
-    console.log('First o_O');
-    setTimeout(()=>{
-        console.log('first script it\'s working');
-    },700);
-}
 function second(){
-    console.log('TWO O_o');
-    setTimeout(()=>{
-        console.log('two script that working');
-    },100);
-}
-function threeth(){
-    console.log('^_^');
-    setTimeout(()=>{
-        console.log('threeth and he working');
+    return new Promise((resolve) => {
+            console.log('TWO O_o');
+            let oK='two script that working it\'s two function';
+        setTimeout(()=>{
+            resolve(console.log(oK));
+                toGoOnWriteAtThePage(oK);
+        },3100);
     });
 }
+function threeth(){
+    return new Promise((resolve) => {
+            console.log('^_^');
+            let oK='threeth and he working resolve three function FINALLY';
+        setTimeout(()=>{
+            resolve(console.log(oK));
+            toGoOnWriteAtThePage(oK);
+        },700);
+    });
+}
+function first(){
+    return new Promise((resolve) => {
+        console.log('First o_O');
+        let oK='O_o resolve First function';
+        setTimeout(()=>{
+           resolve(console.log(oK));
+           toGoOnWriteAtThePage(oK);
+        },3700);
+    });
+};
+
 
 async function asyncAwait(){
+    
+    // let threeth = new Promise((resolve, reject)=>{
+    //     console.log('^_^');
+    //     setTimeout(()=>{
+    //         console.log('threeth and he working');
+    //     });
+    // });
+
+    // let first = new Promise((resolve, reject)=>{
+    //     console.log('First o_O');
+    //     setTimeout(()=>{
+    //         console.log('first script it\'s working');
+    //     },700);
+    // });
+    
+    // let second = new Promise((resolve, reject)=>{
+    //     console.log('TWO O_o');
+    //     setTimeout(()=>{
+    //         console.log('two script that working');
+    //     },100);
+    // });
+    
+
     await first();
-    await threeth();
+
     await second();
-}
+
+    await threeth();
+};
+
 asyncAwait();
+
+
+function toGoOnWriteAtThePage(text){
+    let boxText = document.createElement('p');
+    boxText.innerHTML=text;
+    document.querySelector('main').append(boxText);
+}
+    
